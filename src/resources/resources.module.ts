43669baeb7055a33fa8d1710596a628154943212
BekBoris <38814687+BekBoris@users.noreply.github.com>
2026-04-25 09:09:29 +0400

    development-5

--- src/resources/resources.module.ts
+++ src/resources/resources.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { SecurityModule } from '../security/security.module';
 import { ResourcesController } from './resources.controller';
 import { ResourcesService } from './resources.service';
 
 @Module({
-  imports: [SecurityModule],
   controllers: [ResourcesController],
   providers: [ResourcesService],
   exports: [ResourcesService],
--- src/security/security.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PhiCryptoService } from './phi-crypto.service';
-
-@Module({
-  providers: [PhiCryptoService],
-  exports: [PhiCryptoService],
-})
-export class SecurityModule {}
